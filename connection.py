5c8e0ee7d8a3ef29d00d254d05fbd75787583083
edwin-oei <52173492+edwin-oei@users.noreply.github.com>
2019-06-24 19:37:04 +0200

    Create connection.py

---
+++ connection.py
@@ -0,0 +1,17 @@
+db_ip_address = "" 
+db_user = ""
+db_password = ""
+db_name = ""
+
+event_folder = ""		# Directory where the list of participants (and waiting list) are created and saved.
+receipt_temp_folder = ""	# Directory where receipts are created. Receipts remain in this folder should the system fail to send it.
+receipt_perm_folder = ""	# Directory where receipts are saved after being sent.
+receipt_tutor_folder = ""		# Directory where receipts for the tutors are created and saved 
+tutor_data = ""   # Excel file path where the data of the tutors are stored.
+student_data = ""	# Excel file path where the data of the participants are stored.
+
+email_sender = ""	# Email address of the manager of the international office
+email_sender_password = ""	# Password of the manager's email
+email_hiwi = ""		# Email address of student helper in the office.
+server_out = ""	# Outgoing email server address
+port_out =					#Outgoing port
